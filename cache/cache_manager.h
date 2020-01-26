8b183846cac669f91affe59ee602024a5b3b9348
tomer <tomer.shinar@gmail.com>
2020-01-26 12:30:53 +0200

    fixed cache manager

--- cache/cache_manager.h
+++ cache/cache_manager.h
@@ -7,9 +7,11 @@
 
 template <class P, class S>
 class CacheManager {
+ public:
   virtual bool InCache(P key) = 0;
   virtual S Get(P key) = 0;
   virtual void Add(P key, S value);
+  virtual ~CacheManager() {};
 };
 
 #endif //EX4_CACHE_CACHE_MANAGER_H_
