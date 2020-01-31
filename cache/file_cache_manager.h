6cedeca64160c21cddec09a46ade6d88e166dec5
tomer <tomer.shinar@gmail.com>
2020-01-31 15:50:01 +0200

    fixed template problem

--- cache/file_cache_manager.h
+++ cache/file_cache_manager.h
@@ -6,14 +6,43 @@
 #define EX4_CACHE_FILE_CACHE_MANAGER_H_
 
 #include "cache_manager.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
 
 template <class P, class S>
 class FileCacheManager: public CacheManager<P, S> {
  public:
   FileCacheManager()= default;
-  virtual bool InCache(P key);
-  virtual S Get(P key);
-  virtual void Add(P key, S value);
+  virtual bool InCache(P key) {
+    /**
+  * check if the key is in cache by checking if the file exists
+  */
+    ifstream f(key.hash() + ".txt");
+    return f.good();
+  }
+  virtual S Get(P key) {
+    /***
+ * get the saved value of the key
+ */
+    if (!this->InCache(key)) {
+      throw "no such key in cache";
+    }
+    ifstream f(key.hash() + ".txt");
+    S val;
+    val.ReadFrom(f);
+    f.close();
+    return val;
+  }
+  virtual void Add(P key, S value) override {
+    /**
+ * add the key:value to the cache
+ */
+    ofstream f(key.hash() + ".txt");
+    value.WriteTo(f);
+    f.close();
+  }
 };
 
 #endif //EX4_CACHE_FILE_CACHE_MANAGER_H_
